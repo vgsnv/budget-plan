51b8fa77f750272d47168203d908fc2f940804fa
vgsnv <vgsnv@yandex.ru>
2019-05-16 23:32:44 +0300

    Добавляет webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
-  entry: './src/index.tsx',
+  mode: 'development',
+  entry: { bundle: './src/index.tsx' },
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/dist',
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: '[name].[contenthash].js',
   },
   devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
   },
@@ -14,8 +24,34 @@ module.exports = {
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
     ],
   },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new webpack.HashedModuleIdsPlugin(),
+  ],
+
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
 };
